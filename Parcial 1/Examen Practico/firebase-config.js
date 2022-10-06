bdc9f937570480f98b3a026aa57003c41978ff1c
Irineo Contreras <72529813+Contreras24@users.noreply.github.com>
2022-10-06 14:59:05 -0500

    Add files via upload

---
+++ Parcial 1/Examen Practico/firebase-config.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAUliZuVEPe-yRUgDkWk4ZAmksN_hGMvmc",
+  authDomain: "optativavii-22-4b847.firebaseapp.com",
+  databaseURL: "https://optativavii-22-4b847-default-rtdb.firebaseio.com",
+  projectId: "optativavii-22-4b847",
+  storageBucket: "optativavii-22-4b847.appspot.com",
+  messagingSenderId: "94434148357",
+  appId: "1:94434148357:web:0952402542ac89d678dc6c"
+};
+
